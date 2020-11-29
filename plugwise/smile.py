4986def6e42892c230baca029fa89038e835825b
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-11-29 11:04:20 +0100

    Provide more info in logging

--- plugwise/smile.py
+++ plugwise/smile.py
@@ -353,7 +353,7 @@ class Smile:
                 raise XMLDataMissingError
 
         if self._graph_meas_id is not None:
-            _LOGGER.debug(" Getting graph-data...")
+            _LOGGER.debug(f"Getting graph-data from {self._graph_meas_id} for {self._graph_measurement}.")
             await self.update_graph_data(self._graph_meas_id, self._graph_measurement)
             if self._graph_data is None:
                 _LOGGER.error("Graph data missing")
