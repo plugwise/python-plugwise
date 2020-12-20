3a22273fcfea58d338a8556921db3c2545d13ae0
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-12-20 09:40:16 +0100

    Also format negative numbers

--- plugwise/util.py
+++ plugwise/util.py
@@ -94,11 +94,11 @@ def format_measure(measure, unit):
         if unit == ENERGY_KILO_WATT_HOUR:
             measure = float(measure) / 1000
         try:
-            if float(measure) < 10:
+            if float(abs(measure) < 10:
                 measure = float(f"{round(float(measure), 2):.2f}")
-            elif float(measure) >= 10 and float(measure) < 100:
+            elif float(abs(measure) >= 10 and float(abs(measure)) < 100:
                 measure = float(f"{round(float(measure), 1):.1f}")
-            elif float(measure) >= 100:
+            elif float(abs(measure)) >= 100:
                 measure = int(round(float(measure)))
         except ValueError:
             if measure == "on":
