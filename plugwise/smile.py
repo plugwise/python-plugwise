3e923be158c3995a1cb4e386fb4afcf9725fac3c
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-12-20 14:49:16 +0100

    net_string: output the same format as input

--- plugwise/smile.py
+++ plugwise/smile.py
@@ -913,11 +913,12 @@ class Smile:
                         if "produced" in measurement:
                             diff = -1
                         if net_string not in direct_data:
-                            direct_data[net_string] = float()
-                        direct_data[net_string] += float(f_val * diff)
-                        #direct_data[net_string] = format_measure(
-                        #    str(direct_data[net_string]), attrs[ATTR_UNIT_OF_MEASUREMENT]
-                        #)
+                            if isinstance(f_val, int):
+                                direct_data[net_string] = 0
+                            direct_data[net_string] += int(float(f_val * diff))
+                            else:
+                                direct_data[net_string] = float()
+                            direct_data[net_string] += float(f_val * diff)
 
                     if "gas" in measurement:
                         key_string = f"{measurement}_{log_found}"
