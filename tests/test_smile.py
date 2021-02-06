fe27d9a50c6150866289104741e675939d33c6ec
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2021-02-06 13:20:42 +0100

    Test: import helpers and use

--- tests/test_smile.py
+++ tests/test_smile.py
@@ -19,6 +19,7 @@ import jsonpickle as json
 import pytest
 
 pw_exceptions = importlib.import_module("plugwise.exceptions")
+pw_helpers = importlib.import_module("plugwise.helpers")
 pw_smile = importlib.import_module("plugwise.smile")
 
 pp = PrettyPrinter(indent=8)
@@ -285,7 +286,7 @@ class TestPlugwise:
         self._write_json("get_all_devices", device_list)
         self._write_json("notifications", smile.notifications)
 
-        smile._scan_thermostats()
+        pw_helpers._scan_thermostats()
         location_list = smile.thermo_locs
 
         _LOGGER.info("Gateway id = %s", smile.gateway_id)
