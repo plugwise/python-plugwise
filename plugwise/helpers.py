e290129b40956541a919839ec694dca06f78732b
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2021-02-14 10:05:15 +0100

    Filter invalid Stretch model names

--- plugwise/helpers.py
+++ plugwise/helpers.py
@@ -282,6 +282,8 @@ class Base:
                 mod_type = "electricity_point_meter"
                 module_data = Base.get_module_data(self, appliance, locator, mod_type)
                 appliance_v_name = module_data[0]
+                if appliance_name != "Group Switch":
+                    appliance_name = None
                 if module_data[2] is not None:
                     hw_version = module_data[2].replace("-", "")
                     appliance_model = version_to_model(hw_version)
