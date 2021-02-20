c4b11fa40f38067c392afe1a281efd48af5da332
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2021-02-20 16:07:07 +0100

    Scripts: update according to #65

--- scripts/tests_and_coverage.sh
+++ scripts/tests_and_coverage.sh
@@ -2,4 +2,4 @@
 echo "-----------------------------------------------------------"
 echo "Running plugwise/smile.py through pytest including coverage"
 echo "-----------------------------------------------------------"
-PYTHONPATH=$(pwd) pytest -rpP --log-level debug tests/test_smile.py --cov='.' --no-cov-on-fail
+PYTHONPATH=$(pwd) pytest -rpP --log-level debug tests/test_smile.py --cov='.' --no-cov-on-fail --cov-report term-missing
