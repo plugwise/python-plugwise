49a20253eace83ee94eb6273ccdafb3fcddfe06c
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2021-02-21 13:05:35 +0100

    Switch gateway name and model

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -295,8 +295,8 @@ class SmileHelper:
             if appliance_class == "gateway":
                 self.gateway_id = appliance.attrib["id"]
                 appliance_fw = self.smile_version[0]
-                appliance_model = f"Smile {self.smile_name}"
-                appliance_name = self.smile_name
+                appliance_model = self.smile_name
+                appliance_name = f"Smile {self.smile_name}"
                 appliance_v_name = "Plugwise B.V."
 
             if appliance_class in [
