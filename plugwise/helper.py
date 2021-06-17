58e12d25b3f5c45bec48a23b6154a3eb560f6e4c
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-06-17 17:44:21 +0200

    Return None when no presets found connected to a location

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -531,6 +531,8 @@ class SmileHelper:
         rule_ids = self.rule_ids_by_tag(tag, loc_id)
         if rule_ids is None:
             rule_ids = self.rule_ids_by_name("Thermostat presets", loc_id)
+            if rule_ids is None:
+                return None
 
         for rule_id in rule_ids:
             directives = self._domain_objects.find(f'rule[@id="{rule_id}"]/directives')
