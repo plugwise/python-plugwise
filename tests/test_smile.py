6585ba25f340bf78cd63c669c20ef536c4c3cd4e
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-12-17 20:03:13 +0100

    Move assert after device_test()

--- tests/test_smile.py
+++ tests/test_smile.py
@@ -1457,7 +1457,6 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
         assert smile.smile_type == "power"
         _LOGGER.info(" # Assert version")
         assert smile.smile_version[0] == "3.3.6"
-        _LOGGER.info(" # Assert no master thermostat")
         assert not smile._smile_legacy  # pylint: disable=protected-access
 
         await self.device_test(smile, testdata)
@@ -1676,6 +1675,8 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
 
         assert "3d28a20e17cb47dca210a132463721d5" in smile.notifications
 
+        _LOGGER.info(" # Assert master thermostat")
+        assert smile.sm_thermostat
         await smile.close_connection()
         await self.disconnect(server, client)
 
