683743633bb63f2bd845437d74a93523ab0d1cde
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-12-19 10:45:19 +0100

    Revert "Remove single_master_thermostat from gw_data, no longer used in pw-beta"
    This reverts commit eae23144072f8fd26945255b11b2706c5431e442.

--- plugwise/smile.py
+++ plugwise/smile.py
@@ -92,6 +92,7 @@ class SmileData(SmileHelper):
         self.gw_data["gateway_id"] = self.gateway_id
         self.gw_data["heater_id"] = self._heater_id
         self.gw_data["smile_name"] = self.smile_name
+        self.gw_data["single_master_thermostat"] = self._sm_thermostat
 
     def get_all_devices(self):
         """Determine the devices present from the obtained XML-data."""
