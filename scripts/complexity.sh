3bfcb85e73894c3896f978ef2db9a027310ad346
Tom Scholten <git@scholten.nu>
2021-12-31 10:43:15 +0100

    Improve scripts

--- scripts/complexity.sh
+++ scripts/complexity.sh
@@ -1,5 +1,17 @@
-#!/bin/bash
-echo "-----------------------------"
-echo "Running cyclomatic complexity"
-echo "-----------------------------"
-PYTHONPATH=$(pwd) radon cc plugwise/smile.py plugwise/helper.py tests/test_smile.py -s -nc --no-assert
+#!/usr/bin/env bash
+set -eu
+
+my_path=$(git rev-parse --show-toplevel)
+
+. ${my_path}/scripts/python-venv.sh
+
+if [ -f "${my_venv}/bin/activate" ]; then
+    . "${my_venv}/bin/activate"
+    echo "-----------------------------"
+    echo "Running cyclomatic complexity"
+    echo "-----------------------------"
+    PYTHONPATH=$(pwd) radon cc plugwise/smile.py plugwise/helper.py tests/test_smile.py -s -nc --no-assert
+else
+    echo "Virtualenv available, bailing out"
+    exit 2
+fi
--- scripts/tests_and_coverage.sh
+++ scripts/tests_and_coverage.sh
@@ -1,26 +1,17 @@
 #!/usr/bin/env bash
 set -eu
 
-# Activate pyenv and virtualenv if present, then run the specified command
-
-# pyenv, pyenv-virtualenv
-if [ -s .python-version ]; then
-    PYENV_VERSION=$(head -n 1 .python-version)
-    export PYENV_VERSION
-fi
-
-# other common virtualenvs
 my_path=$(git rev-parse --show-toplevel)
 
-for venv in venv .venv .; do
-  if [ -f "${my_path}/${venv}/bin/activate" ]; then
-    . "${my_path}/${venv}/bin/activate"
+. ${my_path}/scripts/python-venv.sh
+
+if [ -f "${my_venv}/bin/activate" ]; then
+    . "${my_venv}/bin/activate"
     echo "-----------------------------------------------------------"
     echo "Running plugwise/smile.py through pytest including coverage"
     echo "-----------------------------------------------------------"
     PYTHONPATH=$(pwd) pytest -rpP --log-level debug tests/test_smile.py --cov='.' --no-cov-on-fail --cov-report term-missing
-    break
-  fi
-done
-
-
+else
+    echo "Virtualenv available, bailing out"
+    exit 2
+fi
