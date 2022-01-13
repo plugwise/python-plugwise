d9606579629459603248886d1ab3db042f032020
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-01-13 08:29:07 +0100

    Change preset

--- tests/test_smile.py
+++ tests/test_smile.py
@@ -1685,7 +1685,7 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
         _LOGGER.info(" # Assert legacy")
         assert not smile._smile_legacy  # pylint: disable=protected-access
 
-        await self.device_test(smile, testdata)
+        await self.device_test(smile, testdata, True)
         _LOGGER.info(" # Assert master thermostat")
         assert smile._sm_thermostat
 
