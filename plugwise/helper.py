d9bc0ddc603da58607c4039729674b3cddd1f45a
Tom Scholten <git@scholten.nu>
2022-01-13 21:11:10 +0100

    Fix single preset schema, leave out non-schemas

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -159,14 +159,14 @@ def schemas_schedule_temp(schedules, name):
     length = len(schema_list)
     schema_list = sorted(schema_list)
 
-    # Bark on empty schema_list
-    if not schema_list:
-        _LOGGER.error("Current schema %s has no contents", name)
-        raise ConfigurationError
+    #    # Bark on empty schema_list
+    #    if not schema_list:
+    #        _LOGGER.error("Current schema %s has no contents", name)
+    #        raise ConfigurationError
 
     # Schema with less than 2 items
     if length == 1:
-        return schema_list[0][1]
+        return schema_list[0][2]
 
     for i in range(length):
         result_1 = schema_list[i][0]
