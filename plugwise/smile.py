8a116e27e43303c2348d11900636193753a27c02
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-01-23 11:14:22 +0100

    set_schedule_state: replace put by patch and adapt data
    Also, account for the data-differences for Adam and Anna

--- plugwise/smile.py
+++ plugwise/smile.py
@@ -536,20 +536,24 @@ class Smile(SmileComm, SmileData):
             return False
 
         schema_rule_id = next(iter(schema_rule))
-        locator = f'.//rule[@id="{schema_rule_id}"]/directives'
-        directives = etree.tostring(self._domain_objects.find(locator)).decode()
+        locator = f'.//*[@id="{schema_rule_id}"]/template'
+        template_id = self._domain_objects.find(locator).attrib["id"]
+
         info = ""
         if state == "on":
             info = f'<context><zone><location id="{loc_id}" /></zone></context>'
 
+        template = f'<template id="{template_id}" />'
+        if self.smile_name == "Adam":
+            template = '<template tag="zone_preset_based_on_time_and_presence_with_override" />'
+
         uri = f"{RULES};id={schema_rule_id}"
         data = (
             f'<rules><rule id="{schema_rule_id}"><name><![CDATA[{name}]]></name>'
-            '<template tag="zone_preset_based_on_time_and_presence_with_override" />'
-            f"<active>true</active>{directives}<contexts>{info}</contexts></rule></rules>"
+            f"{template}<contexts>{info}</contexts></rule></rules>"
         )
 
-        await self._request(uri, method="put", data=data)
+        await self._request(uri, method="patch", data=data)
 
         return True
 
