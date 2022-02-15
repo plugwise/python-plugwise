11db6cc86ba5d7b8131bda83bcaa5e9e7810af5f
Tom Scholten <git@scholten.nu>
2022-02-15 08:02:36 +0100

    Remove mac obfuscation - Change p1 legacy

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-grep -r --include="*.xml" '<mac_address>' userdata/* | grep -v '01234567' && echo "COMMIT REJECTED, found a mac-address, modify domain_objects and modules and set mac-address to 01234567....! (last 4 digits can remain)" && exit 1
 grep -r --include="*.xml" '<short_id>' userdata/* | grep -v 'abcdefgh' && echo "COMMIT REJECTED, found a real short_id, modify domain_objects and set short_id to abcdefgh!" && exit 1
 grep -r --include="*.xml" '<wifi_ip>' userdata/* | grep -v '_ip></wifi' | grep -v '127.0.0.' && echo "COMMIT REJECTED, found a WIFI ip address, modify domain_objects and set wifi_ip to 127.0.0.1!" && exit 1
 grep -r --include="*.xml" 'lan_ip>' userdata/* | grep -v '_ip></lan' | grep -v '127.0.0.' && echo "COMMIT REJECTED, found a LAN ip address, modify domain_objects and set lan_ip to 127.0.0.1!" && exit 1
@@ -9,6 +8,5 @@ grep -r --include="*.xml" '<longitude>' userdata/* | grep -v '4.49' && echo "COM
 grep -r --include="*.xml" '<latitude>' userdata/* | grep -v '52.21' && echo "COMMIT REJECTED, found your hometown, modify domain_objects and set latitude to 52.21 (that is: Plugwise HQ)!" && exit 1
 grep -r --include="*.xml" '<city>' userdata/* | grep -v 'Sassenheim' && echo "COMMIT REJECTED, found your hometown, modify domain_objects and set city to Sassenheim (that is: Plugwise HQ)!" && exit 1
 grep -r --include="*.xml" '<postal_code>' userdata/* | grep -v '2171' && echo "COMMIT REJECTED, found your hometown, modify domain_objects and set postal_code to 2171 (that is: Plugwise HQ)!" && exit 1
-grep -r --include="*.xml" '<mac>' userdata/* | grep -v '01:23:45:67:' && echo "COMMIT REJECTED, found a mac-address, modify system_status and set mac-address to 01:23:45:67:..:..! (last 4 digits can remain)" && exit 1
 # No problems found
 exit 0
