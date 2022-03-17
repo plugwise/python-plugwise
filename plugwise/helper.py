8e1030110481396cceaa30560f3d3e5fb026dfc3
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-03-17 19:14:04 +0100

    Add self-var for storing allowed modes

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -290,6 +290,7 @@ class SmileHelper:
         """Set the constructor for this class."""
         self._appl_data: dict[str, Any] = {}
         self._appliances: etree | None = None
+        self._allowed_modes: list[str] | None = None
         self._anna_cooling_present: bool = False
         self._cooling_activation_outdoor_temp: float | None = None
         self._cooling_deactivation_threshold: float | None = None
@@ -500,6 +501,7 @@ class SmileHelper:
                     for mode in search.find("allowed_modes"):
                         mode_list.append(mode.text)
                     self._cooling_present = "cooling" in mode_list
+                    self._allowed_modes = mode_list
 
             return appl
 
