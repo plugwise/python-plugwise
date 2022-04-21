dd0f4b3d195dbc6ee0ca54424152472c839af64d
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-04-21 08:21:57 +0200

    Rework output to SmileOutput format

--- plugwise/smile.py
+++ plugwise/smile.py
@@ -480,7 +480,7 @@ class Smile(SmileComm, SmileData):
                             notifs,
                         )
 
-        return [self.gw_data, self.gw_devices]
+        return {"smiledata": [self.gw_data, self.gw_devices]}
 
     async def _set_schedule_state_legacy(self, name: str, status: str) -> None:
         """Helper-function for set_schedule_state()."""
