c6186392a4711cb185a330a7956908e09761959c
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-05-04 11:08:44 +0200

    Add anna_elga_2_cooling testcase, start adapting values

--- tests/test_smile.py
+++ tests/test_smile.py
@@ -2464,6 +2464,103 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
         await self.disconnect(server, client)
 
     @pytest.mark.asyncio
+    async def test_connect_anna_elga_2_cooling(self):
+        """Test a Anna with Elga setup in cooling mode (with missing outdoor temperature - solved)."""
+        testdata = {
+            # Anna
+            "ebd90df1ab334565b5895f37590ccff4": {
+                "dev_class": "thermostat",
+                "firmware": "2018-02-08T11:15:53+01:00",
+                "hardware": "6539-1301-5002",
+                "location": "d3ce834534114348be628b61b26d9220",
+                "model": "Anna",
+                "name": "Anna",
+                "vendor": "Plugwise",
+                "lower_bound": 4,
+                "upper_bound": 30,
+                "resolution": 0.1,
+                "preset_modes": ["away", "no_frost", "vacation", "home", "asleep"],
+                "active_preset": "home",
+                "presets": {
+                    "away": [15.0, 25.0],
+                    "no_frost": [10.0, 30.0],
+                    "vacation": [15.0, 27.0],
+                    "home": [19.5, 23.0],
+                    "asleep": [19.0, 23.0],
+                },
+                "available_schedules": ["Thermostat schedule"],
+                "selected_schedule": "Thermostat schedule",
+                "last_used": "Thermostat schedule",
+                "schedule_temperature": 19.5,
+                "mode": "auto",
+                "sensors": {
+                    "temperature": 20.9,
+                    "setpoint": 19.5,
+                    "illuminance": 0.5,
+                    "cooling_activation_outdoor_temperature": 26.0,
+                    "cooling_deactivation_threshold": 3,
+                },
+            },
+            # Heater central
+            "573c152e7d4f4720878222bd75638f5b": {
+                "dev_class": "heater_central",
+                "location": "d34dfe6ab90b410c98068e75de3eb631",
+                "model": "Generic heater",
+                "name": "OpenTherm",
+                "vendor": "Techneco",
+                "binary_sensors": {
+                    "compressor_state": True,
+                    "dhw_state": False,
+                    "heating_state": False,
+                    "cooling_state": True,
+                    "slave_boiler_state": False,
+                    "flame_state": False,
+                },
+                "sensors": {
+                    "outdoor_air_temperature": 30.0,
+                    "water_temperature": 22.8,
+                    "intended_boiler_temperature": 0.0,
+                    "modulation_level": 0.0,
+                    "return_temperature": 23.4,
+                    "water_pressure": 0.5,
+                },
+                "switches": {"dhw_cm_switch": True},
+            },
+            # Gateway
+            "fb49af122f6e4b0f91267e1cf7666d6f": {
+                "dev_class": "gateway",
+                "firmware": "4.2.1",
+                "hardware": "AME Smile 2.0 board",
+                "location": "d34dfe6ab90b410c98068e75de3eb631",
+                "mac_address": "C4930002FE76",
+                "model": "Anna",
+                "name": "Anna",
+                "vendor": "Plugwise B.V.",
+                "binary_sensors": {"plugwise_notification": False},
+                "sensors": {"outdoor_temperature": 30.0},
+            },
+        }
+
+        self.smile_setup = "anna_elga_2_cooling"
+        server, smile, client = await self.connect_wrapper()
+        assert smile.smile_hostname == "smile000000"
+
+        _LOGGER.info("Basics:")
+        _LOGGER.info(" # Assert type = thermostat")
+        assert smile.smile_type == "thermostat"
+        _LOGGER.info(" # Assert version")
+        assert smile.smile_version[0] == "4.2.1"
+        _LOGGER.info(" # Assert no legacy")
+        assert not smile._smile_legacy
+
+        await self.device_test(smile, testdata)
+        assert self.cooling_present
+        assert not self.notifications
+
+        await smile.close_connection()
+        await self.disconnect(server, client)
+
+    @pytest.mark.asyncio
     async def test_connect_adam_plus_anna_copy_with_error_domain_added(self):
         """Test erroneous domain_objects file from user."""
         testdata = {
