b30f18a70462d0b0b892223e3eba4e948b0dd16d
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-05-13 12:27:21 +0200

    Add no cover

--- tests/test_smile.py
+++ tests/test_smile.py
@@ -562,7 +562,7 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
                         _LOGGER.info("  - succeeded unexpectedly for some reason")
                         raise self.UnexpectedError
             return tinker_schedule_passed
-        _LOGGER.info("- Skipping schedule adjustments")
+        _LOGGER.info("- Skipping schedule adjustments")  # pragma: no cover
 
     @pytest.mark.asyncio
     async def tinker_thermostat(
