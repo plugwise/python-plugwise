237643fc03993d35498a3ad0186421561b04147c
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-07-03 12:48:21 +0200

    Fix asserts

--- tests/test_smile.py
+++ tests/test_smile.py
@@ -3259,9 +3259,8 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
         assert smile.smile_type == "thermostat"
 
         await self.device_test(smile, testdata)
-        assert smile._anna_cooling_present
-        assert smile.anna_cooling_enabled
-        assert not smile._anna_cooling_active
+        assert smile._cooling_present
+        assert smile.adam_cooling_enabled
 
         await smile.close_connection()
         await self.disconnect(server, client)
