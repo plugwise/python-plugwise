67e297f8d1982992e99c2401f2c29f452c2edcc1
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-07-14 20:14:19 +0200

    Test: correct constant

--- tests/test_smile.py
+++ tests/test_smile.py
@@ -403,7 +403,7 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
         for dev_id, details in device_list.items():
             for dev_key, _ in details.items():
                 self.device_items += 1
-                if dev_key in bsw_list or dev_key in pw_constants.ACTUATOR_CLASSES:
+                if dev_key in bsw_list or dev_key in pw_constants.ACTIVE_ACTUATORS:
                     self.device_items -= 1
                     for _ in details[dev_key]:
                         self.device_items += 1
