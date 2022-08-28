bf30213e86130cd7010055680aca68e193fa0cba
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-08-28 10:17:14 +0200

    Define self._schedule_present_state

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -334,6 +334,7 @@ class SmileHelper:
         self._on_off_device = False
         self._opentherm_device = False
         self._outdoor_temp: float
+        self._schedule_present_state: str
         self._sched_setpoints: list[float] | None = None
         self._smile_legacy = False
         self._stretch_v2 = False
