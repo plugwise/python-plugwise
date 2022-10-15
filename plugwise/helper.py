9cf798a988aeb8778d6071711f2798036f405378
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-10-15 13:43:10 +0200

    Optimize power_data_local_format()

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -160,15 +160,13 @@ def power_data_local_format(
     attrs: dict[str, str], key_string: str, val: str
 ) -> float | int | bool:
     """Format power data."""
-    attrs_uom = getattr(attrs, ATTR_UNIT_OF_MEASUREMENT)
-    f_val = format_measure(val, attrs_uom)
-    # Format only P1_MEASUREMENT POWER_WATT values, do not move to util-format_meaure function!
-    if attrs_uom == POWER_WATT:
-        f_val = int(round(float(val)))
+    # Special formatting of P1_MEASUREMENT POWER_WATT values, do not move to util-format_measure() function!
+    if (attrs_uom := getattr(attrs, ATTR_UNIT_OF_MEASUREMENT)) == POWER_WATT:
+        return int(round(float(val)))
     if all(item in key_string for item in ("electricity", "cumulative")):
-        f_val = format_measure(val, ENERGY_KILO_WATT_HOUR)
+        return format_measure(val, ENERGY_KILO_WATT_HOUR)
 
-    return f_val
+    return format_measure(val, attrs_uom)
 
 
 def power_data_energy_diff(
