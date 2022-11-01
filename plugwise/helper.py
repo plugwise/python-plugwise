7aebddaa74430033a55ea88530ef1057ab3b8706
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-11-01 20:19:43 +0100

    More debug

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -929,7 +929,8 @@ class SmileHelper:
                             )
                         ) is not None:
                             device[item] = actuator  # type: ignore [literal-required]
-                            LOGGER.debug("HOI acttuator type: %s", type(actuator))
+                            LOGGER.debug("HOI actuator type: %s", type(actuator))
+                            LOGGER.debug("HOI actuator: %s", actuator)
 
             # Collect availability-status for wireless connected devices to Adam
             self._wireless_availablity(appliance, device)
