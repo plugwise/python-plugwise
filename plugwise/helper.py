9b3647a6f35c7de091d0f000437cc72ee885f389
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-11-04 19:09:30 +0100

    Debug

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -1365,6 +1365,7 @@ class SmileHelper:
         """Helper-function for _get_appliance_data().
         Obtain the toggle state of 'toggle'.
         """
+        LOGGER.debug("HOI 1 %s %s", toggle, name)
         data: DeviceData = {}
         if xml.find("type").text == "heater_central":
             locator = "./actuator_functionalities/toggle_functionality"
@@ -1375,6 +1376,7 @@ class SmileHelper:
                 if (toggle_type := item.find("type")) is not None:
                     if toggle_type.text == toggle:
                         data[name] = item.find("state") == "on"
+            LOGGER.debug("HOI 1a %s", data)
             return data
 
         return None
