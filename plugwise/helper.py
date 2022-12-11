1cf7f62251d0416930d6f8db565c0792ebb6a36e
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-12-11 10:42:51 +0100

    Fix ident

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -1040,7 +1040,8 @@ class SmileHelper:
                     self._cooling_active = (
                         data["cooling_state"] and data["modulation_level"] == 1
                     )
-        LOGGER.debug("HOI heating-state: %s", data["heating_state"])
+
+            LOGGER.debug("HOI heating-state: %s", data["heating_state"])
         self._cleanup_data(data)
 
         return cast(DeviceData, data)
