1b62d6f01549e57a4fe9cf1816a6b5f3bcee8aef
Tom Scholten <git@scholten.nu>
2022-12-27 16:50:57 +0100

    Merge into pyproject.toml

--- plugwise/__init__.py
+++ plugwise/__init__.py
@@ -1,6 +1,8 @@
 """Plugwise module."""
 
-__version__ = "0.27.0"
+import importlib.metadata
+
+__version__ = importlib.metadata.version(__package__ or __name__)
 
 from plugwise.smile import Smile
 from plugwise.stick import Stick
