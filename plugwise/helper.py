f9d9b6abb866c56412048b89a9f07e226816e193
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-03-26 16:36:07 +0200

    Correct broken sensor-naming

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -1233,7 +1233,7 @@ class SmileHelper:
             peak = "off_peak"
         log_found = loc.log_type.split("_")[0]
         loc.key_string = f"{loc.measurement}_{peak}_{log_found}"
-        if "gas" in loc.measurement or "point" in loc.log_type:
+        if "gas" in loc.measurement or "point_meter" in loc.log_type:
             loc.key_string = f"{loc.measurement}_{log_found}"
         if "phase" in loc.measurement:
             loc.key_string = f"{loc.measurement}"
