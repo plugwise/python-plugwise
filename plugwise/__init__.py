a46ef2a80fb82f4a0f014dabc83eadcb4b8b9567
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-08-20 10:51:57 +0200

    Fix

--- plugwise/__init__.py
+++ plugwise/__init__.py
@@ -78,7 +78,7 @@ class SmileData(SmileHelper):
 
         Collect initial data for each device and add to self.gw_data and self.gw_devices.
         """
-        for device_id, _ in self._appl_data.items():
+        for device_id, device in self._appl_data.items():
             self.gw_devices.update({device_id: device})  # type: ignore [misc]
             data = self._get_device_data(device_id)
             # Add plugwise notification binary_sensor to the relevant gateway
