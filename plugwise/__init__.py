2e775549310a316b8d1ce51127793b3863cfd34a
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-09-19 20:05:31 +0200

    Add debug-logging message

--- plugwise/__init__.py
+++ plugwise/__init__.py
@@ -529,6 +529,7 @@ class Smile(SmileComm, SmileData):
         """Perform an incremental update for updating the various device states."""
         # Perform a full update at day-change
         if (new := dt.datetime.now().strftime("%w")) != self._previous:
+            LOGGER.debug("Performing daily full-update, reload the Plugwise integration when a single entity becomes unavailable.")
             self._previous = new
             self.gw_data: GatewayData = {}
             self.gw_devices: dict[str, DeviceData] = {}
