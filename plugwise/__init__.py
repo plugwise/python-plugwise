48ed7b3b2ef9b8d8ea25578e8b19b7d72d6d8f65
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-09-20 17:41:50 +0200

    Add debug-logging for testing

--- plugwise/__init__.py
+++ plugwise/__init__.py
@@ -529,6 +529,7 @@ class Smile(SmileComm, SmileData):
         """Perform an incremental update for updating the various device states."""
         # Perform a full update at day-change
         day_number = dt.datetime.now().strftime("%w")
+        LOGGER.debug("Day-number, Previous-number: %s, %s", day_number, self._previous_day_number)
         if (
             day_number  # pylint: disable=consider-using-assignment-expr
             != self._previous_day_number
