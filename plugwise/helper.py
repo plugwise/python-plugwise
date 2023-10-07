512fc7344cdb9dcdf6107cf775e33c5292d8d17b
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-10-07 19:11:49 +0200

    Remove some debugging

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -881,7 +881,6 @@ class SmileHelper:
         self._count += len(data["sensors"])
         self._count += len(data["switches"])
         self._count += len(data) - 3
-        LOGGER.debug("HOI _appliance_measurements count: %s", self._count)
 
     def _wireless_availablity(self, appliance: etree, data: DeviceData) -> None:
         """Helper-function for _get_measurement_data().
