8caafe7bebd4d88d93a660f3156d773f34bf005f
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2023-10-18 15:50:26 +0200

    Add special single test for turning schedule on based on last_used

--- tests/test_smile.py
+++ tests/test_smile.py
@@ -1953,6 +1953,9 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
         )
         assert result
 
+        # special test-case for turning a schedule back on based onthe  last_used schedule
+        await smile.set_schedule_state("f2bf9048bef64cc5b6d5110154e33c81", "off")
+
         # bad schedule-state test
         result = await self.tinker_thermostat_schedule(
             smile,
