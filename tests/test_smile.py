fd3c8e3ca319618404cde3b90cce0834786e74a2
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-10-19 18:53:58 +0200

    Ffrix

--- tests/test_smile.py
+++ tests/test_smile.py
@@ -738,7 +738,7 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
 
         await self.device_test(smile, "2020-03-22 00:00:01", testdata)
         assert smile.gateway_id == "0000aaaa0000aaaa0000aaaa0000aa00"
-        assert smile._last_active["0000aaaa0000aaaa0000aaaa0000aa00"] == "Thermostat schedule",
+        assert smile._last_active["0000aaaa0000aaaa0000aaaa0000aa00"] == "Thermostat schedule"
         assert smile.device_items == 45
         assert not self.notifications
 
