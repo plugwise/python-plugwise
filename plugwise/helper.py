c02c9caf1960950d924ef2cb59582e22202f682d
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-11-29 08:18:17 +0100

    _get_gateway_mode(): limit to Adam

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -435,7 +435,7 @@ class SmileHelper:
                             self._cooling_present = True
                     self._reg_allowed_modes = reg_mode_list
 
-            # Adam and Anna: check for presence of gateway_modes
+            # Adam: check for presence of gateway_modes
             self._gw_allowed_modes = []
             locator = "./actuator_functionalities/gateway_mode_control_functionality[type='gateway_mode']/allowed_modes"
             if appliance.find(locator) is not None:
@@ -1080,7 +1080,6 @@ class SmileHelper:
         if dev_id == self.gateway_id:
             if self.smile(ADAM):
                 self._get_regulation_mode(appliance, data)
-            if self._is_thermostat and not self._smile_legacy:
                 self._get_gateway_mode(appliance, data)
 
         # Adam & Anna: the Smile outdoor_temperature is present in DOMAIN_OBJECTS and LOCATIONS - under Home
