908fbbd532288566f98990c7bd6acd0806a60c21
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-12-27 14:26:58 +0100

    Update set_gateway_mode()

--- plugwise/__init__.py
+++ plugwise/__init__.py
@@ -871,8 +871,25 @@ class Smile(SmileComm, SmileData):
         if mode not in self._gw_allowed_modes:
             raise PlugwiseError("Plugwise: invalid gateway mode.")
 
+        time_1 = dt.datetime.utcnow()
+        away_time = time_1.isoformat(timespec="milliseconds") + "Z"
+        time_2 = str(dt.date.today() - dt.timedelta(1))
+        vacation_time = time_2 + "T23:00:00.000Z"
+        end_time = "2037-04-21T08:00:53.000Z"
+        if mode == "away":
+            valid = (
+                f"<valid_from>{away_time}</valid_from><valid_to>{end_time}</valid_to>"
+            )
+        if mode == "full":
+            valid = ""
+        if mode == "vacation":
+            valid = (
+                "<valid_from>{vacation_time}</valid_from><valid_to>end_time</valid_to>"
+            )
+
         uri = f"{APPLIANCES};type=gateway/gateway_mode_control"
-        data = f"<gateway_mode_control_functionality><mode>{mode}</mode></gateway_mode_control_functionality>"
+        data = f"<gateway_mode_control_functionality><mode>{mode}</mode>{valid}</gateway_mode_control_functionality>"
+        LOGGER.debug("HOI data: %s", data)
 
         await self._request(uri, method="put", data=data)
 
