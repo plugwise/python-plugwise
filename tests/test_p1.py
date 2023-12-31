8d5d26993712f9c4e5f96a6a5eaeb52c17f4de70
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-12-31 13:46:52 +0100

    Remove legacy-related testcases

--- tests/test_p1.py
+++ tests/test_p1.py
@@ -11,65 +11,6 @@ class TestPlugwiseP1(TestPlugwise):  # pylint: disable=attribute-defined-outside
     """Tests for P1."""
 
     @pytest.mark.asyncio
-    async def test_connect_smile_p1_v2(self):
-        """Test a legacy P1 device."""
-        self.smile_setup = "smile_p1_v2"
-
-        testdata = self.load_testdata(SMILE_TYPE, self.smile_setup)
-        server, smile, client = await self.connect_wrapper()
-        assert smile.smile_hostname == "smile000000"
-
-        self.validate_test_basics(
-            _LOGGER,
-            smile,
-            smile_type="power",
-            smile_version="2.5.9",
-            smile_legacy=True,
-        )
-
-        await self.device_test(smile, "2022-05-16 00:00:01", testdata)
-        assert smile.gateway_id == "aaaa0000aaaa0000aaaa0000aaaa00aa"
-        assert smile.device_items == 26
-        assert not self.notifications
-
-        await smile.close_connection()
-        await self.disconnect(server, client)
-
-    @pytest.mark.asyncio
-    async def test_connect_smile_p1_v2_2(self):
-        """Test another legacy P1 device."""
-        self.smile_setup = "smile_p1_v2_2"
-
-        testdata = self.load_testdata(SMILE_TYPE, self.smile_setup)
-        server, smile, client = await self.connect_wrapper()
-        assert smile.smile_hostname == "smile000000"
-
-        self.validate_test_basics(
-            _LOGGER,
-            smile,
-            smile_type="power",
-            smile_version="2.5.9",
-            smile_legacy=True,
-        )
-
-        await self.device_test(smile, "2022-05-16 00:00:01", testdata)
-        assert smile.device_items == 26
-        assert not self.notifications
-
-        # Now change some data and change directory reading xml from
-        # emulating reading newer dataset after an update_interval
-        testdata_updated = self.load_testdata(
-            SMILE_TYPE, f"{self.smile_setup}_UPDATED_DATA"
-        )
-        self.smile_setup = "updated/smile_p1_v2_2"
-        await self.device_test(
-            smile, "2022-05-16 00:00:01", testdata_updated, initialize=False
-        )
-
-        await smile.close_connection()
-        await self.disconnect(server, client)
-
-    @pytest.mark.asyncio
     async def test_connect_p1v4(self):
         """Test a P1 firmware 4 setup."""
         self.smile_setup = "p1v4"
