2a4009895ea975ed79bd218d1d866a79820dfc0c
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-01-07 16:54:13 +0100

    Disable unused code

--- plugwise/__init__.py
+++ plugwise/__init__.py
@@ -74,7 +74,7 @@ class SmileData(SmileHelper):
             data = self._get_device_data(device_id)
             self._add_or_update_notifications(device_id, device, data)
             device.update(data)
-            self._update_for_cooling(device)
+            # self._update_for_cooling(device)
             remove_empty_platform_dicts(device)
 
     def _add_or_update_notifications(
@@ -94,33 +94,33 @@ class SmileData(SmileHelper):
             data["binary_sensors"]["plugwise_notification"] = bool(self._notifications)
             self._count += 1
 
-    def _update_for_cooling(self, device: DeviceData) -> None:
-        """Helper-function for adding/updating various cooling-related values."""
-        # For Anna and heating + cooling, replace setpoint with setpoint_high/_low
-        if (
-            self.smile(ANNA)
-            and self._cooling_present
-            and device["dev_class"] == "thermostat"
-        ):
-            thermostat = device["thermostat"]
-            sensors = device["sensors"]
-            temp_dict: ActuatorData = {
-                "setpoint_low": thermostat["setpoint"],
-                "setpoint_high": MAX_SETPOINT,
-            }
-            if self._cooling_enabled:
-                temp_dict = {
-                    "setpoint_low": MIN_SETPOINT,
-                    "setpoint_high": thermostat["setpoint"],
-                }
-            thermostat.pop("setpoint")
-            temp_dict.update(thermostat)
-            device["thermostat"] = temp_dict
-            if "setpoint" in sensors:
-                sensors.pop("setpoint")
-            sensors["setpoint_low"] = temp_dict["setpoint_low"]
-            sensors["setpoint_high"] = temp_dict["setpoint_high"]
-            self._count += 2
+#    def _update_for_cooling(self, device: DeviceData) -> None:
+#        """Helper-function for adding/updating various cooling-related values."""
+#        # For Anna and heating + cooling, replace setpoint with setpoint_high/_low
+#        if (
+#            self.smile(ANNA)
+#            and self._cooling_present
+#            and device["dev_class"] == "thermostat"
+#        ):
+#            thermostat = device["thermostat"]
+#            sensors = device["sensors"]
+#            temp_dict: ActuatorData = {
+#                "setpoint_low": thermostat["setpoint"],
+#                "setpoint_high": MAX_SETPOINT,
+#            }
+#            if self._cooling_enabled:
+#                temp_dict = {
+#                    "setpoint_low": MIN_SETPOINT,
+#                    "setpoint_high": thermostat["setpoint"],
+#                }
+#            thermostat.pop("setpoint")
+#            temp_dict.update(thermostat)
+#            device["thermostat"] = temp_dict
+#            if "setpoint" in sensors:
+#                sensors.pop("setpoint")
+#            sensors["setpoint_low"] = temp_dict["setpoint_low"]
+#            sensors["setpoint_high"] = temp_dict["setpoint_high"]
+#            self._count += 2
 
     def get_all_devices(self) -> None:
         """Determine the evices present from the obtained XML-data.
@@ -183,36 +183,36 @@ class SmileData(SmileHelper):
             data["switches"]["relay"] = counter != 0
             self._count += 1
 
-    def _device_data_adam(self, device: DeviceData, data: DeviceData) -> None:
-        """Helper-function for _get_device_data().
-
-        Determine Adam heating-status for on-off heating via valves,
-        available regulations_modes and thermostat control_states.
-        """
-        if self.smile(ADAM):
-            # Indicate heating_state based on valves being open in case of city-provided heating
-            if (
-                device["dev_class"] == "heater_central"
-                and self._on_off_device
-                and isinstance(self._heating_valves(), int)
-            ):
-                data["binary_sensors"]["heating_state"] = self._heating_valves() != 0
-
-            # Show the allowed regulation modes and gateway_modes
-            if device["dev_class"] == "gateway":
-                if self._reg_allowed_modes:
-                    data["regulation_modes"] = self._reg_allowed_modes
-                    self._count += 1
-                if self._gw_allowed_modes:
-                    data["gateway_modes"] = self._gw_allowed_modes
-                    self._count += 1
-
-            # Control_state, only for Adam master thermostats
-            if device["dev_class"] in ZONE_THERMOSTATS:
-                loc_id = device["location"]
-                if ctrl_state := self._control_state(loc_id):
-                    data["control_state"] = ctrl_state
-                    self._count += 1
+#    def _device_data_adam(self, device: DeviceData, data: DeviceData) -> None:
+#        """Helper-function for _get_device_data().
+#
+#        Determine Adam heating-status for on-off heating via valves,
+#        available regulations_modes and thermostat control_states.
+#        """
+#        if self.smile(ADAM):
+#            # Indicate heating_state based on valves being open in case of city-provided heating
+#            if (
+#                device["dev_class"] == "heater_central"
+#                and self._on_off_device
+#                and isinstance(self._heating_valves(), int)
+#            ):
+#                data["binary_sensors"]["heating_state"] = self._heating_valves() != 0
+#
+#            # Show the allowed regulation modes and gateway_modes
+#            if device["dev_class"] == "gateway":
+#                if self._reg_allowed_modes:
+#                    data["regulation_modes"] = self._reg_allowed_modes
+#                    self._count += 1
+#                if self._gw_allowed_modes:
+#                    data["gateway_modes"] = self._gw_allowed_modes
+#                    self._count += 1
+#
+#            # Control_state, only for Adam master thermostats
+#            if device["dev_class"] in ZONE_THERMOSTATS:
+#                loc_id = device["location"]
+#                if ctrl_state := self._control_state(loc_id):
+#                    data["control_state"] = ctrl_state
+#                    self._count += 1
 
     def _device_data_climate(self, device: DeviceData, data: DeviceData) -> None:
         """Helper-function for _get_device_data().
@@ -240,46 +240,46 @@ class SmileData(SmileHelper):
         self._count += 1
         if sel_schedule == NONE:
             data["mode"] = "heat"
-            if self._cooling_present:
-                data["mode"] = "cool" if self.check_reg_mode("cooling") else "heat_cool"
+#            if self._cooling_present:
+#                data["mode"] = "cool" if self.check_reg_mode("cooling") else "heat_cool"
 
-        if self.check_reg_mode("off"):
-            data["mode"] = "off"
+#        if self.check_reg_mode("off"):
+#            data["mode"] = "off"
 
         if NONE not in avail_schedules:
             self._get_schedule_states_with_off(
                 loc_id, avail_schedules, sel_schedule, data
             )
 
-    def check_reg_mode(self, mode: str) -> bool:
-        """Helper-function for device_data_climate()."""
-        gateway = self.gw_devices[self.gateway_id]
-        return (
-            "regulation_modes" in gateway and gateway["select_regulation_mode"] == mode
-        )
+#    def check_reg_mode(self, mode: str) -> bool:
+#        """Helper-function for device_data_climate()."""
+#        gateway = self.gw_devices[self.gateway_id]
+#        return (
+#            "regulation_modes" in gateway and gateway["select_regulation_mode"] == mode
+#        )
 
-    def _get_schedule_states_with_off(
-        self, location: str, schedules: list[str], selected: str, data: DeviceData
-    ) -> None:
-        """Collect schedules with states for each thermostat.
-
-        Also, replace NONE by OFF when none of the schedules are active,
-        only for non-legacy thermostats.
-        """
-        loc_schedule_states: dict[str, str] = {}
-        for schedule in schedules:
-            loc_schedule_states[schedule] = "off"
-            if schedule == selected and data["mode"] == "auto":
-                loc_schedule_states[schedule] = "on"
-        self._schedule_old_states[location] = loc_schedule_states
-
-        all_off = True
-        if not self._smile_legacy:
-            for state in self._schedule_old_states[location].values():
-                if state == "on":
-                    all_off = False
-            if all_off:
-                data["select_schedule"] = OFF
+#    def _get_schedule_states_with_off(
+#        self, location: str, schedules: list[str], selected: str, data: DeviceData
+#    ) -> None:
+#        """Collect schedules with states for each thermostat.
+#
+#        Also, replace NONE by OFF when none of the schedules are active,
+#        only for non-legacy thermostats.
+#        """
+#        loc_schedule_states: dict[str, str] = {}
+#        for schedule in schedules:
+#            loc_schedule_states[schedule] = "off"
+#            if schedule == selected and data["mode"] == "auto":
+#                loc_schedule_states[schedule] = "on"
+#        self._schedule_old_states[location] = loc_schedule_states
+#
+#        all_off = True
+#        if not self._smile_legacy:
+#            for state in self._schedule_old_states[location].values():
+#                if state == "on":
+#                    all_off = False
+#            if all_off:
+#                data["select_schedule"] = OFF
 
     def _check_availability(
         self, device: DeviceData, dev_class: str, data: DeviceData, message: str
@@ -318,8 +318,8 @@ class SmileData(SmileHelper):
 
         # Switching groups data
         self._device_data_switching_group(device, data)
-        # Adam data
-        self._device_data_adam(device, data)
+#        # Adam data
+#        self._device_data_adam(device, data)
         # Skip obtaining data for non master-thermostats
         if device["dev_class"] not in ZONE_THERMOSTATS:
             return data
