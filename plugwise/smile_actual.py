a3a7d2c6b1cd7b8db68b07d4f7c6ce5f10e45767
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-01-28 17:01:41 +0100

    Fix

--- plugwise/smile_actual.py
+++ plugwise/smile_actual.py
@@ -302,7 +302,7 @@ class SmileAPI(SmileComm, SmileData):
         data = f"<thermostat_functionality><setpoint>{temp}</setpoint></thermostat_functionality>"
         await self._request(uri, method="put", data=data)
 
-    async def set_temperature_offset(self, _: str, dev_id: str, offset: float) -> None:
+    async def set_temperature_offset(self, dev_id: str, offset: float) -> None:
         """Set the Temperature offset for thermostats that support this feature."""
         if dev_id not in self.therms_with_offset_func:
             raise PlugwiseError
