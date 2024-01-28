a22c09bc39a563296d222e52078f282ff9c83c11
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-01-28 14:01:06 +0100

    Handle location not in device-dict

--- tests/test_init.py
+++ tests/test_init.py
@@ -417,7 +417,7 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
 
         # location_list_alt = [d.get["location"] for d in list(data.devices.values())]
         _list = list(data.devices.values())
-        _temp_list = [d["location"] for d in _list]
+        _temp_list = [d["location"] for d in _list if "location" in d]
         location_list = list(dict.fromkeys(_temp_list))
 
         _LOGGER.info("Gateway id = %s", data.gateway["gateway_id"])
