efb8a35997aeedd60f59f0e45820d285ccf94cb6
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-01-28 16:50:19 +0100

    Clean up

--- plugwise/smile_actual.py
+++ plugwise/smile_actual.py
@@ -305,9 +305,7 @@ class SmileAPI(SmileComm, SmileData):
     async def set_temperature_offset(self, _: str, dev_id: str, offset: float) -> None:
         """Set the Temperature offset for thermostats that support this feature."""
         if dev_id not in self.therms_with_offset_func:
-            raise PlugwiseError(
-                "Plugwise: this device does not have temperature-offset capability."
-            )
+            raise PlugwiseError
 
         value = str(offset)
         uri = f"{APPLIANCES};id={dev_id}/offset;type=temperature_offset"
