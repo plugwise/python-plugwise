9bc56def88b28f8894e08ee96636c461566cb821
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-01-28 13:21:22 +0100

    Use try-expect to catch error

--- plugwise/__init__.py
+++ plugwise/__init__.py
@@ -349,7 +349,10 @@ class Smile(SmileComm):
         self, appl_id: str, members: list[str] | None, model: str, state: str
     ) -> None:
         """Set the given State of the relevant Switch."""
-        self._smile_api.set_switch_state(appl_id, members, model, state)
+        try:
+            self._smile_api.set_switch_state(appl_id, members, model, state)
+        except PlugwiseError:
+            raise PlugwiseError
 
     async def set_gateway_mode(self, mode: str) -> None:
         """Set the gateway mode."""
