019a2aab0a4aed038b194c43c1746ced10207a3f
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-01-29 20:15:28 +0100

    Remove device_list

--- plugwise/data_legacy.py
+++ plugwise/data_legacy.py
@@ -120,10 +120,6 @@ class SmileLegacyData(SmileHelper):
         """
         self._update_gw_devices()
         self.device_items = self._count
-        self.device_list = []
-        for device in self.gw_devices:
-            self.device_list.append(device)
-
         self.gw_data.update(
             {
                 "gateway_id": self.gateway_id,
