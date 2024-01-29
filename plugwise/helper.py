2a68ac164f5c13a758a6150d3bc67d77bd5ad330
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-01-29 20:09:45 +0100

    Try

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -264,7 +264,7 @@ class SmileHelper:
         self.smile_model: str
         self.smile_name: str
         self.smile_type: str
-        self.smile_version: tuple[str, semver.version.Version]
+        # self.smile_version: tuple[str, semver.version.Version]
         self.smile_zigbee_mac_address: str | None = None
         self.therms_with_offset_func: list[str] = []
 
