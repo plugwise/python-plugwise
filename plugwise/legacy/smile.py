a207d8968688e1ddfb5660d3f7e370232547983a
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-01-30 20:26:43 +0100

    Disable

--- plugwise/legacy/smile.py
+++ plugwise/legacy/smile.py
@@ -113,11 +113,11 @@ class SmileLegacyAPI(SmileComm, SmileLegacyData):
         """
         # Gather all the devices and their initial data
         self._all_appliances()
-        if self._is_thermostat:
-            # Collect a list of thermostats with offset-capability
-            self.therms_with_offset_func = (
-                self._get_appliances_with_offset_functionality()
-            )
+        # if self._is_thermostat:
+        #     # Collect a list of thermostats with offset-capability
+        #     self.therms_with_offset_func = (
+        #         self._get_appliances_with_offset_functionality()
+        #     )
 
         # Collect and add switching- and/or pump-group devices
         if group_data := self._get_group_switches():
