8464fc152fc2838b786c001ab6f95ad55727bf77
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-02-08 14:01:32 +0100

    Update/move comments

--- plugwise/legacy/helper.py
+++ plugwise/legacy/helper.py
@@ -175,15 +175,15 @@ class SmileLegacyHelper:
     def _energy_device_info_finder(self, appliance: etree, appl: Munch) -> Munch:
         """Helper-function for _appliance_info_finder().
 
-        Collect energy device info (Circle, Plug, Stealth): firmware, model and vendor name.
+        Collect energy device info (Smartmeter, Circle, Stealth, etc.): firmware, model and vendor name.
         """
         if self.smile_type in ("power", "stretch"):
             locator = "./services/electricity_point_meter"
             mod_type = "electricity_point_meter"
 
             module_data = self._get_module_data(appliance, locator, mod_type)
-            # Filter appliance without zigbee_mac, it's an orphaned device
             appl.zigbee_mac = module_data["zigbee_mac_address"]
+            # Filter appliance without zigbee_mac, it's an orphaned device
             if appl.zigbee_mac is None and self.smile_type != "power":
                 return None
 
