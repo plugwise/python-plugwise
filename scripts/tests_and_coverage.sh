399087774a8d88fb648bc10bae61963fda2d0e8d
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-02-28 19:01:07 +0100

    Revert back to short test-output

--- scripts/tests_and_coverage.sh
+++ scripts/tests_and_coverage.sh
@@ -30,8 +30,7 @@ set +u
 
 if [ -z "${GITHUB_ACTIONS}" ] || [ "$1" == "test_and_coverage" ] ; then
     # Python tests (rerun with debug if failures)
-    # PYTHONPATH=$(pwd) pytest -qx tests/ --cov='.' --no-cov-on-fail --cov-report term-missing || 
-    PYTHONPATH=$(pwd) pytest -xrpP --log-level debug tests/
+    PYTHONPATH=$(pwd) pytest -qx tests/ --cov='.' --no-cov-on-fail --cov-report term-missing || PYTHONPATH=$(pwd) pytest -xrpP --log-level debug tests/
 fi
 
 if [ -z "${GITHUB_ACTIONS}" ] || [ "$1" == "linting" ] ; then
