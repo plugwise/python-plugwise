9d64d3b9c598a9dfef26ea5948dbd132c05f3334
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-03-16 10:39:44 +0100

    Init strings with NONE

--- plugwise/__init__.py
+++ plugwise/__init__.py
@@ -80,9 +80,9 @@ class Smile(SmileComm):
         self.smile_hw_version: str | None = None
         self.smile_legacy = False
         self.smile_mac_address: str | None = None
-        self.smile_model: str
-        self.smile_name: str
-        self.smile_type: str
+        self.smile_model: str = NONE
+        self.smile_name: str = NONE
+        self.smile_type: str = NONE
         self.smile_version: tuple[str, semver.version.Version]
         self.smile_zigbee_mac_address: str | None = None
 
