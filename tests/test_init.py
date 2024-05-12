7fc703d68f27e1fd475beffa6548d56d03ec1014
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-05-12 19:46:18 +0200

    Adapt relevant testcases

--- tests/test_init.py
+++ tests/test_init.py
@@ -873,11 +873,10 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
     async def tinker_max_boiler_temp(smile):
         """Change max boiler temp setpoint to test functionality."""
         new_temp = 60.0
-        dev_id = None
         _LOGGER.info("- Adjusting temperature to %s", new_temp)
         for test in ["maximum_boiler_temperature", "bogus_temperature"]:
             try:
-                await smile.set_number_setpoint(test, dev_id, new_temp)
+                await smile.set_number_setpoint(test, new_temp)
                 _LOGGER.info("  + tinker_max_boiler_temp worked as intended")
             except pw_exceptions.PlugwiseError:
                 _LOGGER.info("  + tinker_max_boiler_temp failed as intended")
@@ -888,7 +887,7 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
         new_offset = 1.0
         _LOGGER.info("- Adjusting temperature offset to %s", new_offset)
         try:
-            await smile.set_temperature_offset("dummy", dev_id, new_offset)
+            await smile.set_number_setpoint("dummy", new_offset, dev_id)
             _LOGGER.info("  + tinker_temp_offset worked as intended")
             return True
         except pw_exceptions.PlugwiseError:
