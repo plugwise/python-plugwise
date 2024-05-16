aa571850eb5c666de5ab5a1fe1786b8d3337a0e1
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-05-16 12:40:20 +0200

    Improvements

--- plugwise/__init__.py
+++ plugwise/__init__.py
@@ -329,12 +329,12 @@ class Smile(SmileComm):
         key: str,
         temperature: float,
     ) -> None:
-        """Set the max. Boiler or DHW setpoint on the Central Heating boiler."""
+        """Set the maximum boiler- or DHW-setpoint on the Central Heating boiler or the temperature-offset on a Thermostat."""
         await self._smile_api.set_number(dev_id, key, temperature)
 
-    async def set_temperature_offset(self, _: str, dev_id: str, offset: float) -> None:
+    async def set_temperature_offset(self, dev_id: str, offset: float) -> None:
         """Set the Temperature offset for thermostats that support this feature."""
-        await self._smile_api.set_temperature_offset(dev_id, offset)
+        await self._smile_api.set_offset(dev_id, offset)
 
     async def set_switch_state(
         self, appl_id: str, members: list[str] | None, model: str, state: str
