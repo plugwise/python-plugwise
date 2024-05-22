9f12d8708a4fcb721a313fd678ac6e86cbbb2ba8
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-05-22 19:25:33 +0200

    Remove double code

--- plugwise/smile.py
+++ plugwise/smile.py
@@ -154,39 +154,6 @@ class SmileAPI(SmileComm, SmileData):
         GATEWAY_REBOOT = "/core/gateways;@reboot"
         await self._request(GATEWAY_REBOOT, method="put", data=None)
 
-    async def set_dhw_mode(self, mode: str) -> None:
-        """Set the domestic hot water heating regulation mode."""
-        if mode not in self._dhw_allowed_modes:
-            raise PlugwiseError("Plugwise: invalid dhw mode.")
-
-        uri = f"{APPLIANCES};type=heater_central/domestic_hot_water_mode_control"
-        data = f"<domestic_hot_water_mode_control_functionality><mode>{mode}</mode></domestic_hot_water_mode_control_functionality>"
-
-        await self._request(uri, method="put", data=data)
-
-    async def set_gateway_mode(self, mode: str) -> None:
-        """Set the gateway mode."""
-        if mode not in self._gw_allowed_modes:
-            raise PlugwiseError("Plugwise: invalid gateway mode.")
-
-        end_time = "2037-04-21T08:00:53.000Z"
-        valid = ""
-        if mode == "away":
-            time_1 = self._domain_objects.find("./gateway/time").text
-            away_time = dt.datetime.fromisoformat(time_1).astimezone(dt.UTC).isoformat(timespec="milliseconds").replace("+00:00", "Z")
-            valid = (
-                f"<valid_from>{away_time}</valid_from><valid_to>{end_time}</valid_to>"
-            )
-        if mode == "vacation":
-            time_2 = str(dt.date.today() - dt.timedelta(1))
-            vacation_time = time_2 + "T23:00:00.000Z"
-            valid = f"<valid_from>{vacation_time}</valid_from><valid_to>{end_time}</valid_to>"
-
-        uri = f"{APPLIANCES};id={self.gateway_id}/gateway_mode_control"
-        data = f"<gateway_mode_control_functionality><mode>{mode}</mode>{valid}</gateway_mode_control_functionality>"
-
-        await self._request(uri, method="put", data=data)
-
     async def set_number(
         self,
         dev_id: str,
