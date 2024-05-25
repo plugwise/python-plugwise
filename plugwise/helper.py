0d9774c302ddcd3eed760d7dd8a0cac494dc14de
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-05-25 14:24:19 +0200

    Ruffed

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -169,7 +169,9 @@ class SmileComm:
             LOGGER.error("%s", msg)
             raise InvalidAuthentication
 
-        if not (result := await resp.text()) or ("<error>" in result and "Not started" not in result):
+        if not (result := await resp.text()) or (
+            "<error>" in result and "Not started" not in result
+        ):
             LOGGER.warning("Smile response empty or error in %s", result)
             raise ResponseError
 
