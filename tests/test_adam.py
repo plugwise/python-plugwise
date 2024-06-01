a9d945f20df12b9517a916dfde5d9eaa5bea4482
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-06-01 10:55:15 +0200

    Fix 2

--- tests/test_adam.py
+++ tests/test_adam.py
@@ -78,7 +78,7 @@ class TestPlugwiseAdam(TestPlugwise):  # pylint: disable=attribute-defined-outsi
         try:
             await smile.delete_notification()
             notification_deletion = False  # pragma: no cover
-        except pw_exceptions.ConnectionFailedError:
+        except pw_exceptions.PlugwiseError:
             notification_deletion = True
 
         assert notification_deletion
