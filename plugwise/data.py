2bb1581d1c593f94712c86c0f5826f5a68223766
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-08-25 12:49:18 +0200

    Remove own added battery-low limits, not working
    Plugwise changes the message to a warning notification when the battery-level becomes critically low.

--- plugwise/data.py
+++ plugwise/data.py
@@ -67,10 +67,7 @@ class SmileData(SmileHelper):
                 mac_list
                 and "low_battery" in device["binary_sensors"]
                 and device["zigbee_mac_address"] in mac_list
-                and (
-                    (device["dev_class"] in ("thermo_sensor", "thermostatic_radiator_valve") and device["sensors"]["battery"] < 30)
-                    or (device["dev_class"] in ("zone_thermometer", "zone_thermostat") and device["sensors"]["battery"] < 15)
-                )
+                and device["dev_class"] in ("thermo_sensor", "thermostatic_radiator_valve", "zone_thermometer", "zone_thermostat")
             )
             if is_battery_low:
                 device["binary_sensors"]["low_battery"] = True
