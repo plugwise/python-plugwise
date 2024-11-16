b53898de3b5dd2d3a7e43e2c46c13eecaf1e8d06
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-11-16 10:15:39 +0100

    Update device-items assert for added device

--- tests/test_adam.py
+++ tests/test_adam.py
@@ -36,7 +36,7 @@ class TestPlugwiseAdam(TestPlugwise):  # pylint: disable=attribute-defined-outsi
         assert smile.gateway_id == "da224107914542988a88561b4452b0f6"
         assert smile._last_active["f2bf9048bef64cc5b6d5110154e33c81"] == "Weekschema"
         assert smile._last_active["f871b8c4d63549319221e294e4f88074"] == "Badkamer"
-        assert self.device_items == 157
+        assert self.device_items == 166
         assert self.device_list == [
             "da224107914542988a88561b4452b0f6",
             "056ee145a816487eaa69243c3280f8bf",
