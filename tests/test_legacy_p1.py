3efafca9ffdcee41ceef16c420ceb8b7d77caf39
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-11-17 09:01:41 +0100

    Limit test-cases for now

--- tests/test_legacy_p1.py
+++ tests/test_legacy_p1.py
@@ -34,36 +34,3 @@ class TestPlugwiseP1(TestPlugwise):  # pylint: disable=attribute-defined-outside
         await smile.close_connection()
         await self.disconnect(server, client)
 
-    @pytest.mark.asyncio
-    async def test_connect_smile_p1_v2_2(self):
-        """Test another legacy P1 device."""
-        self.smile_setup = "smile_p1_v2_2"
-
-        testdata = self.load_testdata(SMILE_TYPE, self.smile_setup)
-        server, smile, client = await self.connect_legacy_wrapper()
-        assert smile.smile_hostname == "smile000000"
-
-        self.validate_test_basics(
-            _LOGGER,
-            smile,
-            smile_type="power",
-            smile_version="2.5.9",
-            smile_legacy=True,
-        )
-
-        await self.device_test(smile, "2022-05-16 00:00:01", testdata)
-        assert self.device_items == 26
-
-        # Now change some data and change directory reading xml from
-        # emulating reading newer dataset after an update_interval
-        testdata_updated = self.load_testdata(
-            SMILE_TYPE, f"{self.smile_setup}_UPDATED_DATA"
-        )
-        self.smile_setup = "updated/smile_p1_v2_2"
-        await self.device_test(
-            smile, "2022-05-16 00:00:01", testdata_updated, initialize=False
-        )
-
-        await smile.close_connection()
-        await self.disconnect(server, client)
-
--- tests/test_p1.py
+++ tests/test_p1.py
@@ -11,30 +11,6 @@ class TestPlugwiseP1(TestPlugwise):  # pylint: disable=attribute-defined-outside
     """Tests for P1."""
 
     @pytest.mark.asyncio
-    async def test_connect_p1v4(self):
-        """Test a P1 firmware 4 setup."""
-        self.smile_setup = "p1v4"
-
-        testdata = self.load_testdata(SMILE_TYPE, self.smile_setup)
-        server, smile, client = await self.connect_wrapper()
-        assert smile.smile_hostname == "smile000000"
-
-        self.validate_test_basics(
-            _LOGGER,
-            smile,
-            smile_type="power",
-            smile_version="4.1.1",
-        )
-
-        await self.device_test(smile, "2022-05-16 00:00:01", testdata)
-        assert smile.gateway_id == "a455b61e52394b2db5081ce025a430f3"
-        assert self.device_items == 30
-        assert "97a04c0c263049b29350a660b4cdd01e" in self.notifications
-
-        await smile.close_connection()
-        await self.disconnect(server, client)
-
-    @pytest.mark.asyncio
     async def test_connect_p1v4_442_single(self):
         """Test a P1 firmware 4.4 single-phase setup."""
         self.smile_setup = "p1v4_442_single"
@@ -64,30 +40,6 @@ class TestPlugwiseP1(TestPlugwise):  # pylint: disable=attribute-defined-outside
         await self.device_test(
             smile, "2022-05-16 00:00:01", testdata_updated, initialize=False
         )
-
-        await smile.close_connection()
-        await self.disconnect(server, client)
-
-    @pytest.mark.asyncio
-    async def test_connect_p1v4_442_triple(self):
-        """Test a P1 firmware 4 3-phase setup."""
-        self.smile_setup = "p1v4_442_triple"
-
-        testdata = self.load_testdata(SMILE_TYPE, self.smile_setup)
-        server, smile, client = await self.connect_wrapper()
-        assert smile.smile_hostname == "smile000000"
-
-        self.validate_test_basics(
-            _LOGGER,
-            smile,
-            smile_type="power",
-            smile_version="4.4.2",
-        )
-
-        await self.device_test(smile, "2022-05-16 00:00:01", testdata)
-        assert smile.gateway_id == "03e65b16e4b247a29ae0d75a78cb492e"
-        assert self.device_items == 41
-        assert self.notifications
 
         await smile.close_connection()
         await self.disconnect(server, client)
