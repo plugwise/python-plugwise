9125fe6045343899b14e2fb721221d1ac8d61105
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-11-22 10:21:01 +0100

    Another fix

--- scripts/manual_fixtures.py
+++ scripts/manual_fixtures.py
@@ -223,7 +223,7 @@ m_adam_heating["entities"]["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"]
 m_adam_heating["entities"]["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"][
     "heating_state"
 ] = True
-m_adam_cooling["entities"]["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"][
+m_adam_heating["entities"]["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"][
     "flame_state"
 ] = False
 m_adam_heating["entities"]["056ee145a816487eaa69243c3280f8bf"]["sensors"][
