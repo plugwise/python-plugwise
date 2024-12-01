517f6d5f6306ed92dfbd8c18cea7b2aa407f08fb
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-12-01 20:18:59 +0100

    Fix lowest version

--- plugwise/data.py
+++ plugwise/data.py
@@ -166,7 +166,7 @@ class SmileData(SmileHelper):
             if str(ctrl_state) in ("cooling", "heating", "preheating"):
                 data["control_state"] = str(ctrl_state)
                 self._count += 1
-        elif self.smile_version > version.parse("3.6.0"):
+        elif self.smile_version >= version.parse("3.2.0"):
             data["control_state"] = "idle"
             self._count += 1
 
