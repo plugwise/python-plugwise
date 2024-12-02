0fbf1f63cdba8dbfa67206fa4e2777edd826f9b2
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-12-02 19:16:41 +0100

    Add thermostat/setpoint as zone-sensor

--- plugwise/constants.py
+++ plugwise/constants.py
@@ -196,6 +196,7 @@ ZONE_MEASUREMENTS: Final[dict[str, DATA | UOM]] = {
     "electricity_produced": UOM(POWER_WATT),
     "relay": UOM(NONE),
     "temperature": UOM(TEMP_CELSIUS),  # HA Core thermostat current_temperature
+    "thermostat": DATA("setpoint", TEMP_CELSIUS),  # HA Core thermostat setpoint
 }
 
 # Literals
