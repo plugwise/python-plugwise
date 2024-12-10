464ce2512763b5aab42b15588aad0eee9f84e47c
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-12-10 19:49:49 +0100

    Ruff format fix

--- plugwise/legacy/smile.py
+++ plugwise/legacy/smile.py
@@ -126,7 +126,9 @@ class SmileLegacyAPI(SmileLegacyData):
                 # Detect failed data-retrieval
                 _ = self.gw_entities[self.gateway_id]["location"]
             except KeyError as err:  # pragma: no cover
-                raise DataMissingError("No (full) Plugwise legacy data received") from err
+                raise DataMissingError(
+                    "No (full) Plugwise legacy data received"
+                ) from err
         # Otherwise perform an incremental update
         else:
             try:
