8b9bd067a0c8082d886811ad62b6a1107737ee2a
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-02-18 17:43:54 +0100

    Compact

--- plugwise/data.py
+++ plugwise/data.py
@@ -332,11 +332,8 @@ class SmileData(SmileHelper):
         """
         self._schedule_old_states[location] = {}
         for schedule in schedules:
-            self._schedule_old_states[location][schedule] = (
-                "on"
-                if schedule == selected and data["climate_mode"] == "auto"
-                else "off"
-            )
+            active: bool = schedule == selected and data["climate_mode"] == "auto"
+            self._schedule_old_states[location][schedule] = "on" if active else "off"
 
         all_off = True
         for state in self._schedule_old_states[location].values():
