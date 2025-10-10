d532e13b4622ea42b90b84b39fa111879ecbc057
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-10-10 17:16:00 +0200

    Add ThermoZone for Anna

--- plugwise/smile.py
+++ plugwise/smile.py
@@ -10,7 +10,6 @@ import datetime as dt
 from typing import Any, cast
 
 from plugwise.constants import (
-    ADAM,
     ANNA,
     APPLIANCES,
     DOMAIN_OBJECTS,
@@ -113,8 +112,7 @@ class SmileAPI(SmileData):
             self.therms_with_offset_func = (
                 self._get_appliances_with_offset_functionality()
             )
-            if self.check_name(ADAM):
-                self._scan_thermostats()
+            self._scan_thermostats()
 
         if group_data := self._get_group_switches():
             self.gw_entities.update(group_data)
