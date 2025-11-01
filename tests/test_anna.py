33649a9e65eb57bcebabd8ab9e0f07c75b88a413
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-11-01 17:39:09 +0100

    Update anna_p1 entity_items assert

--- tests/test_anna.py
+++ tests/test_anna.py
@@ -551,7 +551,7 @@ class TestPlugwiseAnna(TestPlugwise):  # pylint: disable=attribute-defined-outsi
         )
 
         await self.device_test(api, "2025-11-2 00:00:01", testdata)
-        assert self.entity_items == 12
+        assert self.entity_items == 55
 
         await api.close_connection()
         await self.disconnect(server, client)
