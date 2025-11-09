5fd3d429a6c14ad7b08b1c777c7eba100976c103
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-11-09 10:00:06 +0100

    Fix time_out route

--- tests/test_init.py
+++ tests/test_init.py
@@ -126,7 +126,7 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
         else:
             app.router.add_route("POST", CORE_GATEWAYS_TAIL, self.smile_timeout)
             app.router.add_route("PUT", CORE_LOCATIONS_TAIL, self.smile_timeout)
-            app.router.add_route("POST", CORE_LOCATIONS_TAIL, self.smile_http_accept)
+            app.router.add_route("POST", CORE_LOCATIONS_TAIL, self.smile_timeout)
             app.router.add_route("PUT", CORE_RULES_TAIL, self.smile_timeout)
             app.router.add_route("PUT", CORE_APPLIANCES_TAIL, self.smile_timeout)
             app.router.add_route("DELETE", CORE_NOTIFICATIONS_TAIL, self.smile_timeout)
