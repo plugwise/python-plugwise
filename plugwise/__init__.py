f3892a6e49305c507a9a575160d9cadd03d8f8d8
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-11-24 08:19:46 +0100

    Fix

--- plugwise/__init__.py
+++ plugwise/__init__.py
@@ -119,7 +119,7 @@ class Smile(SmileComm):
     async def connect(self) -> Version:
         """Connect to the Plugwise Gateway and determine its name, type, version, and other data."""
         result = await self._request(DOMAIN_OBJECTS)
-        result_dict = xmltodict.parse(result)
+        result_dict = xmltodict.parse(DOMAIN_OBJECTS)
         LOGGER.debug("HOI result_dict: %s", result_dict)
         # Work-around for Stretch fw 2.7.18
         if not (vendor_names := result.findall("./module/vendor_name")):
