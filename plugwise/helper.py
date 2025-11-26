088cde8c6d27e7cf29d5b7cdf7712b8c20518ca3
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-11-26 17:35:55 +0100

    Save various updates

--- plugwise/helper.py
+++ plugwise/helper.py
@@ -382,7 +382,9 @@ class SmileHelper(SmileCommon):
         measurements: dict[str, UOM],
     ) -> None:
         """Collect group sensors."""
-        if (group := self._domain_objects.find(f'./group[@id="{entity_id}"]')) is not None:
+        if (
+            group := self._domain_objects.find(f'./group[@id="{entity_id}"]')
+        ) is not None:
             for measurement, attrs in measurements.items():
                 locator = f'.//logs/point_log[type="{measurement}"]/period/measurement'
                 if (group_meas_loc := group.find(locator)) is None:
