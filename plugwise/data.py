9397522b3f28815cdbe0c98c3da5bfc8ccc7087d
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-12-30 16:12:30 +0100

    Reorganize _get_entity_data()

--- plugwise/data.py
+++ plugwise/data.py
@@ -183,30 +183,27 @@ class SmileData(SmileHelper):
         Provide entity-data, based on appliance_id (= entity_id).
         """
         self._get_measurement_data(entity_id, entity)
-
-        # Check availability of wired-connected entities
-        # Smartmeter
-        self._check_availability(
-            entity, "smartmeter", "P1 does not seem to be connected"
-        )
-        # OpenTherm entity
-        if entity["name"] != "OnOff":
-            self._check_availability(
-                entity, "heater_central", "no OpenTherm communication"
-            )
-        # Zigbee node availability
-        self._update_zigbee_availability(entity)
-
-        # Switching groups data
-        self._entity_switching_group(entity)
         # Adam data
         if self.check_name(ADAM):
             self._get_adam_data(entity)
-
+            # Update ZigBee node availability
+            self._update_zigbee_availability(entity)
+            # Update switching-group status
+            self._entity_switching_group(entity)
         # Thermostat data for Anna (presets, temperatures etc)
         if self.check_name(ANNA) and entity["dev_class"] == "thermostat":
             self._climate_data(entity_id, entity)
             self._get_anna_control_state(entity)
+        # Check availability of wired-connected entities,
+        # Smartmeter:
+        self._check_availability(
+            entity, "smartmeter", "P1 does not seem to be connected"
+        )
+        # OpenTherm entity:
+        if entity["name"] != "OnOff":
+            self._check_availability(
+                entity, "heater_central", "no OpenTherm communication"
+            )
 
     def _check_availability(
         self, entity: GwEntityData, dev_class: str, message: str
