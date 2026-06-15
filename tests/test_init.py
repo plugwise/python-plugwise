ecc4acec8464a6c96c4a5fdf4862e6db8cac5d33
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-06-15 19:30:52 +0200

    Fix typo

--- tests/test_init.py
+++ tests/test_init.py
@@ -859,7 +859,7 @@ class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
                 mode = mode[1:]
             _LOGGER.info("%s", f"- Adjusting dhw mode to {mode}{warning}")
             try:
-                await api.set_dhw_mode(appliance, lenght, mode)
+                await api.set_dhw_mode(appliance, length, mode)
                 _LOGGER.info("  + tinker_dhw_mode worked as intended")
                 tinker_dhw_mode_passed = True
             except pw_exceptions.PlugwiseError:
