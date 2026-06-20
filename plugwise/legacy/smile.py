eefb304869eb5e0aecf8f4edf2bc894eb523dbfb
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-06-20 09:50:20 +0200

    Adapt legacy set_switch_state() output typing

--- plugwise/legacy/smile.py
+++ plugwise/legacy/smile.py
@@ -223,7 +223,7 @@ class SmileLegacyAPI(SmileLegacyData):
 
     async def set_switch_state(
         self, appl_id: str, members: list[str] | None, model: str, state: str
-    ) -> bool:
+    ) -> bool | None:
         """Set the given state of the relevant switch.
 
         For individual switches, sets the state directly.
@@ -231,7 +231,7 @@ class SmileLegacyAPI(SmileLegacyData):
         For switch-locks, sets the lock state using a different data format.
         Return the requested state when successful, the current state otherwise.
         """
-        current_state = self.gw_entities[appl_id]["switches"]["relay"]
+        current_state = self.gw_entities[appl_id]["switches"].get("relay")
         requested_state = state == STATE_ON
         switch = Munch()
         switch.actuator = "actuator_functionalities"
