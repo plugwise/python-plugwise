1fdfc526b20e44a5dc0ee4cee10e2744e921812f
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-07-11 09:10:12 +0200

    Revert some changes in legacy helper.py

--- plugwise/legacy/helper.py
+++ plugwise/legacy/helper.py
@@ -342,10 +342,20 @@ class SmileLegacyHelper(SmileCommon):
         self._count = count_data_items(self._count, data)
 
     def _get_actuator_functionalities(
-        self, xml: etree.Element, data: GwEntityData
+        self,
+        xml: etree.Element,
+        data: GwEntityData,
+        entity: GwEntityData,
     ) -> None:
         """Helper-function for _get_measurement_data()."""
         for item in ACTIVE_ACTUATORS:
+            # Skip max_dhw_temperature, not initially valid,
+            # skip thermostat for thermo_sensors
+            if item == "max_dhw_temperature" or (
+                item == "thermostat" and entity["dev_class"] == "thermo_sensor"
+            ):
+                continue
+
             temp_dict: ActuatorData = {}
             functionality = "thermostat_functionality"
 
