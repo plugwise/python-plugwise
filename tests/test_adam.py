5bf90abced067523405007d9dd2e599e0d365be9
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-07-11 19:43:46 +0200

    Update entity_items count

--- tests/test_adam.py
+++ tests/test_adam.py
@@ -400,7 +400,7 @@ class TestPlugwiseAdam(TestPlugwise):  # pylint: disable=attribute-defined-outsi
 
         test_items = await self.device_test(api, "2020-03-22 00:00:01", testdata)
         assert api.gateway_id == "b128b4bbbd1f47e9bf4d756e8fb5ee94"
-        assert self.entity_items == 83
+        assert self.entity_items == 85
         assert test_items == self.entity_items
         assert "6fb89e35caeb4b1cb275184895202d84" in self.notifications
 
